aaba3bfcafde6dca0fd19e738c54c544af2e5e4f
R4yan <45489155+R4yGM@users.noreply.github.com>
2020-03-07 14:53:27 +0100

    Create info.go

---
+++ info/info.go
@@ -0,0 +1,75 @@
+package info
+
+import (
+//  "io/ioutil"
+//  "encoding/json"
+  //"fmt"
+//  "os"
+)
+var ver = "0.1"
+var owner = "R4yan"
+var github = "https://github.com/R4yGM/netscanner"
+var help = "\nnetscanner 0.1\n     \nUsage: netscanner <command>\n\nCommands:\n\n   sp,scanport - scan a singular port: usage sp,scanport <protocol> <hostname> <port> (ex. scanport tcp 127.0.01 80)\n   in,initscan - scan the first 1023 ports: usage in,initscan <protocol> <hostname> <savefilebool>(ex. initscan udp 127.0.0.1 false)\n   cs,completescan - scan all the ports: usage cs,completescan <protocol> <hostname> <savefilebool>(ex. initscan tcp 127.0.0.1 true)\n		help - shows this text\n   version - show the current version of the program\n 	info,information - shows some information about the program"
+
+type Info struct {
+	Version string `json:"version"`
+	Owner   string    `json:"owner"`
+  Github string `json:"github"`
+  Help string  `json:"help"`
+}
+
+func Version()(tex string){
+//  file, _ := ioutil.ReadFile("info.json")
+
+/*  	data := Info{}
+
+  	_ = json.Unmarshal([]byte(file), &data)*/
+    //fmt.Println(ver)
+    tex = ver
+    return tex
+}
+func Owner()(tex string){
+
+//  file, _ := ioutil.ReadFile("info.json")
+
+  /*  data := Info{}
+
+    _ = json.Unmarshal([]byte(file), &data)*/
+      //  fmt.Println(owner)
+    tex = owner
+    return tex
+
+}
+func Github()(tex string){
+
+//  file, _ := ioutil.ReadFile("./info.json")
+
+    /*data := Info{}
+
+    _ = json.Unmarshal([]byte(file), &data)
+    tex = data.Github*/
+    tex = github
+    //fmt.Println(tex)
+    return tex
+
+}
+func Helper()(tex string){
+
+  //jsonFile, err := os.Open("github.com/R4yGM/netscanner/data.json")
+/*
+     if err != nil {
+         fmt.Println(err)
+     }
+
+     defer jsonFile.Close()
+
+     var in Info
+
+
+      responseData, err := ioutil.ReadAll(jsonFile)
+      json.Unmarshal(responseData, &in)
+     fmt.Println(in.Help)*/
+     tex = help
+     return
+
+}
