1912bdd9694bea145310295cb7bc7597b1513403
R4yan <45489155+R4yGM@users.noreply.github.com>
2020-03-23 20:31:08 +0100

    Create netmask.go

---
+++ netmask/netmask.go
@@ -0,0 +1,37 @@
+package netmask
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func Hosts(cidr string) ([]string, error) {
+	ip, ipnet, err := net.ParseCIDR(cidr)
+	if err != nil {
+		return nil, err
+	}
+
+	var ips []string
+	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
+		ips = append(ips, ip.String())
+	}
+
+	lenIPs := len(ips)
+	switch {
+	case lenIPs < 2:
+		return ips, nil
+
+	default:
+	return ips[1 : len(ips)-1], nil
+	}
+}
+
+func inc(ip net.IP) {
+	for j := len(ip) - 1; j >= 0; j-- {
+		ip[j]++
+		if ip[j] > 0 {
+			break
+		}
+	}
+}
